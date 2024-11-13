09316cecdb42ae225ee9033e1884133b36a9d89e
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-13 09:53:35 +0000

    feat(info): added the information to render from the LanguagesList page

---
+++ data/data.tsx
@@ -0,0 +1,17 @@
+export const info = [
+    {
+        photo: require('../assets/images/logo-react.png') ,
+        title: 'React Native',
+        description:'Estabilidad y rendimiento nativo en Android.'
+    },
+    {
+        photo: require('../assets/images/logo-jav.png') ,
+        title: 'Java',
+        description:'Desarrollo multiplataforma eficiente y ahorro de tiempo.'
+    },
+    {
+        photo: require('../assets/images/logo-java.png') ,
+        title: 'JavaScript',
+        description:'Flexibilidad y compatibilidad con múltiples entornos de desarrollo.'
+    },
+]
