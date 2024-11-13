1df253ca15c62abdca1bcd074edcac40af004375
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-13 09:49:17 +0000

    feat(Qr): Modification of the Qr page by adding the components, images and styles of the page

--- app/provider/(tabs)/qr.tsx
+++ app/provider/(tabs)/qr.tsx
@@ -1,14 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import QRCode from 'react-native-qrcode-svg';
+import { Link } from 'expo-router';
 
 const Qr = () => {
   return (
-    <View>
-      <Text>Qr</Text>
+    <View style={styles.containerQrCode} >
+      <Text style={styles.linesTitle}>___________________________</Text>
+      <Text style={styles.titleInfo}>Visita mi Repositorio</Text>
+      <Text style={styles.description}>Para explorar Proyectos y Soluciones en los que he trabajado.</Text>
+      <Text style={styles.linesTitle}>___________________________</Text>
+      <Link style={styles.link} href="/">
+        <Text style={styles.buttomText}>Inicio</Text>
+      </Link>
+      <View style={styles.qrCode}>
+        <QRCode value="https://github.com/adhernea" />
+      </View>
     </View>
   )
 }
 
 export default Qr
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  link: {
+    backgroundColor: 'purple',
+    width: 90,
+    borderRadius: 50,
+    paddingTop: 10,
+    paddingBottom: 10,
+    alignSelf: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 15
+  },
+  buttomText: {
+    color: 'white',
+    textAlign: 'center'
+  },
+  linesTitle: {
+    alignSelf: 'center',
+  },
+  titleInfo: {
+    fontSize: 35,
+    alignSelf: 'center',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  containerQrCode: {
+    alignItems: 'center',
+    marginTop:-50
+
+  },
+  description: {
+    textAlign: 'center',
+    width: 290
+  },
+  qrCode: {
+    marginTop: 30,
+  }
+})
