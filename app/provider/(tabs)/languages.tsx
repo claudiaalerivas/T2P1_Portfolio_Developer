dee63f922c0b920ccc2412582773e1b95599fb5a
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-13 09:39:33 +0000

    feat(navigation): Creation of empty languages ​​and qr pages, in addition the _layout file was created that defines where the stack is located with the redirection to the created pages

---
+++ app/provider/(tabs)/languages.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const languagesList = () => {
+  return (
+    <View>
+      <Text>languagesList</Text>
+    </View>
+  )
+}
+
+export default languagesList
+
+const styles = StyleSheet.create({})
