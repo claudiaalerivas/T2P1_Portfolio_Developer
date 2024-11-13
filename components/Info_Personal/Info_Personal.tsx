d922401552f753dcd368fa066450920c69f3867c
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-13 09:13:28 +0000

    feat(components): coponent creation Info_Personal

---
+++ components/Info_Personal/Info_Personal.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Info_Personal = () => {
+  return (
+    <View>
+      <Text>Info_Personal</Text>
+    </View>
+  )
+}
+
+export default Info_Personal
+
+const styles = StyleSheet.create({})
