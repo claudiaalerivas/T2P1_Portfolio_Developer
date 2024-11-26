0c8f752041b0bc9fb55cd92be59f27fbe6578565
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-26 14:25:26 +0000

    feat: list with data loaded by default for testing and added type of list and images of the product categories

---
+++ data/list.tsx
@@ -0,0 +1,26 @@
+import { info } from "../types/Type";
+
+export const list: info[] = [
+    {
+        name:  "Pescado",
+        cantidad: 1,
+        image: require('../assets/images/fished.png'),
+        category: "Pescados",
+        price: 2,
+    },
+    {
+        name: "Manzana",
+        cantidad: 1,
+        image: require('../assets/images/fruitsVegetables.png'),
+        category: "Verdura",
+        price: 2,
+    },
+    {
+        name: "Pancito Fránces",
+        cantidad: 2,
+        image: require('../assets/images/pan.png'),
+        category: "Panaderia",
+        price: 2,
+    },
+    
+]
