9e1ce03471a3aba4312b8b7106f707a403ba2904
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2024-11-27 01:32:48 +0000

    feat(initialForm): add default list

---
+++ data/list.tsx
@@ -0,0 +1,20 @@
+import { info } from "../types/type"
+
+export const initialForm: info[] = [
+    {
+      name: 'Leche',
+      cantidad: 2,
+      image: require('../assets/images/drinks.png'),
+      category: 'Bebidas',
+      price: 1.5,
+      obtained: false,
+    }, 
+    {
+      name: 'Solomillo',
+      cantidad: 2,
+      image: require('../assets/images/meat.png'),
+      category: 'Bebidas',
+      price: 1.5,
+      obtained: true,
+    }, 
+  ]
