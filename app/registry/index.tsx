b35fc0f9b6fc1ea5fc2a2edd73798440f76ecb31
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2025-01-16 21:09:20 +0000

    feat(RegisterPage):A new form was created with values ​​to register a user and rename project in the package-lock.json

--- app/registry/index.tsx
+++ app/registry/index.tsx
@@ -1,72 +1,80 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
-import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const RegistryUser = () => {
   const emptyForm = {
-    id: '',
     name: '',
     email: '',
     password: '',
-  }
+  };
 
-  const initialListUsers = [
-    {
-      id: '123',
-      name: 'Claudia',
-      email: 'a@a.a',
-      password: '123',
-    },
-  ]
   const [form, setForm] = useState(emptyForm);
-  const [usersList, setUsersList] = useState(initialListUsers);
-  const handleInputChange = (field: string, value: string) => {
-    setForm({ ...form, [field]: value });
-  };
+
   const sendForm = () => {
-    setUsersList(() => [...usersList, form]);
-    setForm(emptyForm)
+    setForm(emptyForm);
   };
+
 
   return (
     <View style={styles.container}>
-      <View style={styles.containerTitle}>
+      <View style={styles.containerInputs}>
+        <AntDesign name="adduser"style={styles.title}  size={80} color="purple" />
         <Text style={styles.title}>Registro de Usuario</Text>
+        <Text>Nombre:</Text>
+        <TextInput
+          style={styles.input}
+          value={form.name}
+          onChangeText={(text) => setForm({ ...form, name: text })}
+          placeholder="Estefania"
+          returnKeyType="done"
+        />
+        <Text>Email:</Text>
+        <TextInput
+          style={styles.input}
+          value={form.email}
+          onChangeText={(text) => setForm({ ...form, email: text })}
+          placeholder="estefania@gmail.com"
+          keyboardType="email-address"
+          returnKeyType="done"
+        />
+        <Text>Contraseña:</Text>
+        <TextInput
+          style={styles.input}
+          value={form.password}
+          onChangeText={(text) => setForm({ ...form, password: text })}
+          secureTextEntry={true}
+          returnKeyType="done"
+        />
+        <View>
+          <Button
+            title="Guardar"
+            onPress={sendForm}
+            disabled={form.name === '' || form.email === '' || form.password === '' || form.email.length < 8 || form.email.includes('ABCDEFG')}
+          />
+        </View>
       </View>
-      <Text>Nombre:</Text>
-      <TextInput
-        style={styles.input}
-        value={form.name}
-        onChangeText={(text) => handleInputChange('name', text)}
-        placeholder="Estefania"
-      />
-      <Text>Email:</Text>
-      <TextInput
-        style={styles.input}
-        value={form.name}
-        onChangeText={(text) => handleInputChange('email', text)}
-        placeholder="estefania@gmail.com"
-      />
-      <Text>Contraseña:</Text>
-      <TextInput
-        style={styles.input}
-        value={form.name}
-        onChangeText={(text) => handleInputChange('password', text)}
-        placeholder="*********"
-        secureTextEntry={true}
-      />
-      <View>
-        <Button title="Guardar" onPress={() => sendForm()} disabled={form.name == '' || form.email == '' || form.password == '' || form.email.length < 5} />
-      </View>
-
     </View>
-  )
-}
+  );
+};
 
-export default RegistryUser
+export default RegistryUser;
 
 const styles = StyleSheet.create({
   container: {
-    width: 100
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  containerInputs: {
+    borderTopWidth: 5,
+    borderWidth: 1,
+    borderColor: 'purple',
+    borderRadius: 15,
+    padding: 20,
+    width: '80%',
+    backgroundColor: 'white',
   },
   input: {
     borderWidth: 1,
@@ -75,10 +83,9 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
   },
-  containerTitle: {
-    alignItems: 'center'
-  },
   title: {
     fontSize: 30,
+    marginBottom: 20,
+    textAlign: 'center',
   },
-})
+});
