aa5e114d396c23d2e2b007572aacae4cc999fd91
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2025-02-09 14:23:33 +0000

    fix(image-servise): getImages() function modification

--- services/image-service.ts
+++ services/image-service.ts
@@ -2,16 +2,22 @@ import asyncStorageService from "./async-storage-service";
 
 const API_URL = "http://192.168.0.156:5000/auth";
 
-async function getImages() {
-
-  await fetch(`${API_URL}/images/get-all`, {
-    headers: {
-      "Aplication": "",
-      "Authorization": "user-token",
-    }
-  })
-    .then(r => r.json())
+async function getImages<T>(): Promise<T | undefined> {
+  try {
+    const response = await fetch(`${API_URL}/images/get-all`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "user-token",
+      }
+    });
+    const data = await response.json(); 
+    return data;
+  } catch (e) {
+    console.log(`Fetch Error: ${e}`);
+  }
 }
+
 
 const imageService = {
   getImages
