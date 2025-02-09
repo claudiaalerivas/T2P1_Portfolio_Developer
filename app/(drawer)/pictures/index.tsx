8f3539f8e85187328d65bbc1c4e784012da01af8
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2025-02-09 14:48:57 +0000

    feat(PicturesPage): added functionality to list a user's images using the service

--- app/(drawer)/pictures/index.tsx
+++ app/(drawer)/pictures/index.tsx
@@ -1,25 +1,46 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import Camera from '../../../components/Camera/Camera';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import imageService from '../../../services/image-service';
 
 const PicturesPage = () => {
-  const [picture, setPicture] = useState<string>("");
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const data = await imageService.getImages<string[]>(); 
+      if (data) {
+        setImages(data);
+      } else {
+        setImages([]); 
+      }
+    };
+    fetchImages();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        {picture == "" ? null : (
-          <Image
-            style={styles.imageStyle}
-            source={{ uri: `data:image/jpg;base64,${picture}` }}
-          />
-        )}
-      </View>
+      {images.length === 0 ? (
+        <Text style = {styles.title}> No tienes imágenes guardadas</Text>
+      ) : (
+        <FlatList
+          data={images}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={3} 
+          renderItem={({ item }) => (
+            <Image
+              source={{ uri: `data:image/jpg;base64,${item}` }}
+              style={styles.thumbnail} 
+            />
+          )}
+        />
+      )}
     </View>
-  )
+  );
+  
+
 }
 
 export default PicturesPage
-
 const styles = StyleSheet.create({
 
   imageContainer: {
@@ -40,6 +61,20 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    alignItems: "center",
     justifyContent: "center",
+    padding: 10,
+  },
+  thumbnail: {
+    width: 100,
+    height: 100,
+    margin: 5,
+    borderRadius: 10,
+  },
+  title: {
+    alignSelf:'center', 
+    fontSize: 15, 
+    color: 'purple' 
   }
+
 })
