0d9c810a0d3a298532ca1f9a51422efb2fd5d129
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2025-02-11 12:57:44 +0000

    fix(asyncStorageService): modification of getUser function of asyncStorageService

--- services/async-storage-service.ts
+++ services/async-storage-service.ts
@@ -3,6 +3,17 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 const KEYS = {
   userToken: 'user-token'
 }
+
+const getUser = async  (key: string): Promise<string | null> => {
+  try {
+    const token = await AsyncStorage.getItem(key);
+    return token
+  } catch (e) {
+    console.log(`AsyncStorage Error: ${e}`);
+  }
+  return null;
+}
+
 async function saveUser<T>(key: string, value: T): Promise<void | null | undefined> {
   try {
     const jsonValue = JSON.stringify(value);
@@ -11,16 +22,7 @@ async function saveUser<T>(key: string, value: T): Promise<void | null | undefin
     console.log(`AsyncStorage Error: ${e}`);
   }
 }
-const getUser = async  (key: string): Promise<any | null> => {
-  try {
-    const jsonValue = await AsyncStorage.getItem(key);
-    return jsonValue != null ? JSON.parse(jsonValue) : null;
-  } catch (e) {
-    console.log(`AsyncStorage Error: ${e}`);
-  }
 
-  return null;
-}
 const deleteTokenUser = async  (key: string): Promise<any | null> => {
   try {
     const v = await AsyncStorage.removeItem(key);
