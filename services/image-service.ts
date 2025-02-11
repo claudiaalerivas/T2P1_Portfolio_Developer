6d673d2b10123e4f5a2099796667a25f7b8a4650
claudiaalerivas <usr3860@salesianos-lacuesta.net>
2025-02-11 13:01:28 +0000

    fix(imageService): modification of getImages, saveImageToApi functions and new function added: deleteImageToApi

--- services/image-service.ts
+++ services/image-service.ts
@@ -1,35 +1,89 @@
+import axios from "axios";
 import asyncStorageService from "./async-storage-service";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_URL = "http://192.168.0.123:5000/auth";
+const API_URL = "http://172.20.10.3:5000";
 
-async function getImages<T>(): Promise<T | undefined> {
+
+const getImages = async () => {
+  const token = await asyncStorageService.getUser(asyncStorageService.KEYS.userToken);
+
+  if (token == null) {
+    console.error("No se encontró un token válido en AsyncStorage.");
+    return;
+  }
+  const cleanedToken = token!.replace(/['"]+/g, '');
   try {
-    const response = await fetch(`${API_URL}/images/get-all`, {
-      method: "GET",
+    const response = await axios.get(`${API_URL}/images/get-all`, {
+      headers: {
+        Authorization: `Bearer ${cleanedToken}`,
+      },
+    });
+    return response.data.object;
+
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Error en la solicitud:", error.response?.data || error.message);
+    } else {
+      console.log('Error fetching images:', error);
+    }
+    return [];
+  }
+};
+
+const deleteImageToApi = async (id: number, height: number, width: number, base64Image: string): Promise<void> => {
+  try {
+    const tokenObject = await asyncStorageService.getUser(asyncStorageService.KEYS.userToken);
+
+    if (tokenObject == null) {
+      console.error("No se encontró un token válido en AsyncStorage.");
+      return;
+    }
+    const cleanedToken = tokenObject!.replace(/['"]+/g, '');
+
+    const response = await fetch(`${API_URL}/images/${id}`, {
+      method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "user-token",
-      }
+        "Authorization": `Bearer ${cleanedToken}`
+      },
+      body: JSON.stringify({
+        id: id,
+        height: height,
+        width: width,
+        encodedData: base64Image,
+      })
     });
-    const data = await response.json(); 
-    return data;
-  } catch (e) {
-    console.log(`Fetch Error: ${e}`);
+    const result = await response.json();
+    console.log("Imagen subida con éxito:", result);
+  } catch (error) {
+    console.error("Error al subir la imagen:", error);
+    throw error;
   }
-}
+};
 
-const saveImageToApi = async (base64Image: string): Promise<void> => {
+
+const saveImageToApi = async (height: number, width: number, base64Image: string): Promise<void> => {
   try {
-    const response = await fetch(`${API_URL}/images/upload`, {
+    const tokenObject = await asyncStorageService.getUser(asyncStorageService.KEYS.userToken);
+
+    if (tokenObject == null) {
+      console.error("No se encontró un token válido en AsyncStorage.");
+      return;
+    }
+
+    const cleanedToken = tokenObject!.replace(/['"]+/g, '');
+
+    const response = await fetch(`${API_URL}/images/save`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "user-token"
+        "Authorization": `Bearer ${cleanedToken}`
       },
       body: JSON.stringify({
-        height: height,        
-        width: width,          
-        encodedData: base64Image, 
+        height: height,
+        width: width,
+        encodedData: base64Image,
       })
     });
     const result = await response.json();
@@ -42,7 +96,8 @@ const saveImageToApi = async (base64Image: string): Promise<void> => {
 
 const imageService = {
   getImages,
-  saveImageToApi
+  saveImageToApi,
+  deleteImageToApi
 };
 
 export default imageService;
